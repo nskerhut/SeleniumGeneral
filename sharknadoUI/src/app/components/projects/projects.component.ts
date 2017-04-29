162cdc7e480ab9739332b12742bbed711a1e033e
Johnathan Alexander <johnathanalexander@Johnathan-Alexander-MacBook-Pro.local>
2017-04-29 14:06:48 -0500

    added charge code details and contact details to projectdetails form.  modified project.component.ts for updates and bug fixes.

--- sharknadoUI/src/app/components/projects/projects.component.ts
+++ sharknadoUI/src/app/components/projects/projects.component.ts
@@ -73,37 +73,116 @@ constructor(
        this.getAllEmployees();
        this.getListOfProject();
   }
-public enableForm(){
-    //Enables fields in project details form.
-    var name2=<HTMLInputElement> document.getElementById("name2");
-    var chargecode=<HTMLInputElement> document.getElementById("chargecode2");
-    var manager=<HTMLInputElement> document.getElementById("manager2");
-    var email=<HTMLInputElement> document.getElementById("email2");
-    
-    if(name2.readOnly == true){
-        name2.readOnly=false;
-        chargecode.readOnly=false;
-        manager.readOnly=false;
-        email.readOnly=false;
-    }else{
-        name2.readOnly=true;
-    chargecode.readOnly=true;
-    manager.readOnly=true;
-    email.readOnly=true; 
+    public enableForm(){
+        //Enables fields in project details form.
+        
+        //if statement required to determine which form id is active
+        
+        
+        
+        
+        
+        if(document.getElementById("projectDetailsForm").style.display == "block"){
+            var name2=<HTMLInputElement> document.getElementById("name2");
+            var chargecode=<HTMLInputElement> document.getElementById("chargecode2");
+            var manager=<HTMLInputElement> document.getElementById("manager2");
+            var email=<HTMLInputElement> document.getElementById("email2");
+            
+            if(name2.readOnly == true){
+                name2.readOnly=false;
+                chargecode.readOnly=false;
+                manager.readOnly=false;
+                email.readOnly=false;
+            }else{
+                name2.readOnly=true;
+            chargecode.readOnly=true;
+            manager.readOnly=true;
+            email.readOnly=true; 
+            }
+        }
+        else if(document.getElementById("projectContactForm").style.display == "block"){
+            
+        }
+        else{
+            
+        }
+        
+       
     }
-    
+    public setFieldReadOnly(){
+        //projectDetailsForm
+        var name2=<HTMLInputElement> document.getElementById("name2");
+        var chargecode=<HTMLInputElement> document.getElementById("chargecode2");
+        var manager=<HTMLInputElement> document.getElementById("manager2");
+        var email=<HTMLInputElement> document.getElementById("email2");
+        
+        //projectContactForm
+        
+        
+        //projectChargeCodeForm
+        
+        
+        
+        
+        if(name2.readOnly == false){
+            name2.readOnly = true;
+            chargecode.readOnly = true;
+            manager.readOnly = true;
+            email.readOnly = true;
+        }
+        
+    }
+    public removeElements(){
+        //Remove "Project Details" Tab
+        document.getElementById("projectDetailsForm").style.display = "none";
+        
+        //Remove "Charge Codes" Tab
+        document.getElementById("projectChargeCodeForm").style.display = "none";
+
+        //Remove "Contacts" Tab
+        document.getElementById("projectContactForm").style.display = "none";
+        }
    
+    public showContacts(){
+        document.getElementById("projectContactForm").style.display = "block";
+        document.getElementById("projDetailsHead").textContent = "View Project Contacts";
+        
+        
+        //also need to get the values for charge codes and make sure they are set to a variable
+        
+        
+        //set unneeded items to not visible
+        
+        }
+    public showChargeCodes(){
+        document.getElementById("projDetailsHead").textContent = "View Project Charge Codes";
+        document.getElementById("projectChargeCodeForm").style.display = "block";
+        }
+    public submitChanges(){
+        this.normalizeProjectForm();
+        
+        //save changes to db
     }
-public setFieldReadOnly(){
-    var name2=<HTMLInputElement> document.getElementById("name2");
-    var chargecode=<HTMLInputElement> document.getElementById("chargecode2");
-    var manager=<HTMLInputElement> document.getElementById("manager2");
-    var email=<HTMLInputElement> document.getElementById("email2");
-    
-    name2.readOnly = true;
-    chargecode.readOnly = true;
-    manager.readOnly = true;
-    email.readOnly = true;
+    public normalizeProjectForm(){
+        document.getElementById("projectContactForm").style.display = "none";
+        document.getElementById("projectChargeCodeForm").style.display = "none";
+        
+        document.getElementById("projDetailsHead").textContent = "View Project Details Form";
+        
+        document.getElementById("projectDetailsForm").style.display = "block";
+        
+        var name2=<HTMLInputElement> document.getElementById("name2");
+        var chargecode=<HTMLInputElement> document.getElementById("chargecode2");
+        var manager=<HTMLInputElement> document.getElementById("manager2");
+        var email=<HTMLInputElement> document.getElementById("email2");
+        
+        if(name2.readOnly == false){
+            name2.readOnly = true;
+            chargecode.readOnly = true;
+            manager.readOnly = true;
+            email.readOnly = true;
+        }
+        
     }
 
 }
