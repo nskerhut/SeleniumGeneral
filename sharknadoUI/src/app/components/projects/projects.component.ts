a28ab9f9b20d7c9f5d5d33f4dda45b851974fd43
Sebastian Skerhut <Sebastian.Skerhut@csra.com>
2017-05-24 15:55:01 -0500

    Drag and Drop better with Allocation awareness

--- sharknadoUI/src/app/components/projects/projects.component.ts
+++ sharknadoUI/src/app/components/projects/projects.component.ts
@@ -5,7 +5,7 @@ import { Project } from '../../model/project';
 import { EventEmitter, Input, Output } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { DndModule } from 'ng2-dnd';
-
+import { AssignedProject } from '../../model/assignedproject';
 
 @Component( {
     selector: 'app-projects, demo-modal-static',
@@ -51,20 +51,41 @@ export class ProjectsComponent implements OnInit {
     }
     moveEmployeeToEmployeeList( $event: any ) {
         let newEmployee: Employee = $event.dragData;
-        let projectFrom: Project = newEmployee.AssignedProject;
+        let projectFrom: Project = this.projectFrom;
 
         if ( projectFrom != null ) {
             projectFrom.employees = projectFrom.employees.filter( item => item !== newEmployee );
             console.log( `project From(%s) employee list`, projectFrom.projectId);
         }
+        
+    //newEmployee.Allocation = 40 - newEmployee.Allocation;
+        newEmployee.TotalAllocation = 0;
+        this.unassignedEmployeeList = this.unassignedEmployeeList.filter(x => x !== newEmployee);
+        this.unassignedEmployeeList.unshift(newEmployee);
+        
+        
     }
-    addEmployeeToProject( $event: any, projectId: number ) {
+    
+    projectFrom: Project;
+    dragStart(p:Project, e:Employee){
+        if( p != null){
+            console.log("removing %s from %s", e.First_Name, p.Project_Name)
+            //e.assignedProject = e.assignedProject.filter(x => x !== p);
+            this.projectFrom = p;
+            //p.employees = p.employees.filter(x => x !== e);
+            
+        } else {
+           // this.unassignedEmployeeList = this.unassignedEmployeeList.filter(x => x !== e);
+            //this.unassignedEmployeeList.push(e);
+        }
+    }
+    addEmployeeToProject( $event: any, project: Project ) {
+        
         let newEmployee: Employee = $event.dragData;
-        let projectTo: Project = this.projectList[projectId];
-        let id: number = projectTo.projectId;
+        let projectTo: Project = project;
+        let projectFrom: Project = this.projectFrom;
 
-        let projectFrom: Project = newEmployee.AssignedProject;
-
+        console.log("adding %s to %s", newEmployee.First_Name, project.Project_Name);
         if ( projectFrom != null ) {
             projectFrom.employees = projectFrom.employees.filter( item => item !== newEmployee );
             console.log( `project From(%s) To(%s)`, projectFrom.projectId, projectTo.projectId );
@@ -73,13 +94,19 @@ export class ProjectsComponent implements OnInit {
         //Move Employee to bottom allocated.
         this.unassignedEmployeeList = this.unassignedEmployeeList.filter( employee => employee !== newEmployee );
         this.unassignedEmployeeList.push( newEmployee );
-
-
+       
         if ( projectTo.employees == null )
             projectTo.employees = new Array<Employee>();
 
         projectTo.employees.push( newEmployee );
-        newEmployee.AssignedProject = projectTo;
+        let newProject: AssignedProject = projectTo as AssignedProject;
+        
+        //newProject.AllocationHrs = 40; //TODO Set a real amount of time
+        newEmployee.TotalAllocation = 40;
+        if(newEmployee.assignedProject == null)
+            newEmployee.assignedProject = new Array<AssignedProject>();
+        newEmployee.assignedProject.push(newProject);
+        
         console.log( "employees list: %s", projectTo.employees );
 
     }
