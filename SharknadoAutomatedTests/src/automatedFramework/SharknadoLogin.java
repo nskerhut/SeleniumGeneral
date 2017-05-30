26d2388bf24dc7e172a0b1376970d082185d8454
Ryan Durham <lacrossechamp@bellsouth.net>
2017-05-30 01:54:52 -0600

    Work in Progress automation for SHRK-145 and SHRK-165

---
+++ SharknadoAutomatedTests/src/automatedFramework/SharknadoLogin.java
@@ -0,0 +1,79 @@
+package automatedFramework;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class SharknadoLogin {
+	
+	public static WebDriver GetDriver(String browser)
+	{
+		WebDriver driver;
+		
+		if (browser == "ie")
+		{
+			System.setProperty("webdriver.ie.driver", "C:\\Program Files\\Java\\seleniumDrivers\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+			return driver;
+		}
+		else if(browser == "firefox")
+		{
+			
+		}
+		else if(browser == "chrome")
+		{
+			
+		}
+		else 
+		{
+			System.setProperty("webdriver.ie.driver", "C:\\Program Files\\Java\\seleniumDrivers\\IEDriverServer.exe");
+		}
+		driver = new InternetExplorerDriver();
+		return driver;
+		
+	}
+	
+	public static void login(WebDriver driver)
+	{
+		//navigate to home page
+		driver.navigate().to("http://Localhost:3000");
+		
+		//sleep for 2 secs
+		sleep(2000);
+		
+		//Click the login button 
+		driver.findElement(By.xpath("//a[text() = 'Login']")).click();
+		
+		//sleep for 2 secs
+		sleep(2000);
+		
+		//clear and send username to  username text field
+		driver.findElement(By.id("userName")).clear();
+		driver.findElement(By.id("userName")).sendKeys("Mitch");
+		
+		//clear and send password to password text field
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("Owens");
+		
+		//click submit to sign in
+		driver.findElement(By.xpath("//input[@class = 'btn-yes btn']")).click();
+		
+		//sleep for 2 secs
+		sleep(2000);
+		
+		
+	}
+	
+	public static void sleep(int x)
+	{
+		try        
+		{
+		    Thread.sleep(x);
+		} 
+		catch(InterruptedException ex) 
+		{
+		    Thread.currentThread().interrupt();
+		}
+		
+	}
+}
+
---
+++ SharknadoAutomatedTests/src/automatedTests/SHRK_145_165.java
@@ -0,0 +1,46 @@
+package automatedTests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+import automatedFramework.*;
+
+public class SHRK_145_165 
+{
+	public static void main(String[] args)
+	{
+	
+		// Variables 
+		WebDriver driver;
+		
+		
+		
+		// get the driver and login
+		driver = SharknadoLogin.GetDriver("ie");
+		SharknadoLogin.login(driver);
+	
+		Actions Builder = new Actions(driver);
+		
+		//navigate to projects page
+		driver.findElement(By.xpath("//a[text() = 'Projects']")).click();
+		
+		SharknadoLogin.sleep(15000);
+		
+		WebElement avaYorel = driver.findElement(By.xpath("//li[@class = 'list-group-item']//div[text() = 'Yorel Baker']"));
+		WebElement projBob = driver.findElement(By.xpath("//div[@class = 'panel-info' and contains(.,'bob') ]/div[@id = 'Projpanel']"));
+		
+		//WebElement projBobYorel = driver.findElement(By.xpath("//div[@class = 'panel-info' and contains(.,'bob') ]//div[text() = 'Yorel Baker']"));
+		//WebElement avaEmployees = driver.findElement(By.xpath("//li[@class = 'list-group-item']"));
+		Builder.moveToElement(avaYorel).clickAndHold().moveToElement(projBob).release().build().perform();
+
+		SharknadoLogin.sleep(3000);
+		
+		//dragEmployee.dragAndDrop(projBobYorel, avaEmployees).perform();
+		
+		driver.quit();
+		
+	
+	}
+}
